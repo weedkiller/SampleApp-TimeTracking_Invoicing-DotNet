1f19c93100562a509cebe07a1c6cac029e66e336
smathilakath <sumod_madhavan@intuit.com>
2015-03-26 21:51:40 +0530

    [*] Sync Service bug fix

--- Clean.sql
+++ Clean.sql
@@ -3,3 +3,7 @@
 select * from OAuthTokens
 select * from Employee
 truncate table Employee
+
+UPDATE Employee
+SET GivenName='William', FamilyName='Worfing'
+WHERE Id='1';
--- TimeTracking/Models/Service/SyncService.cs
+++ TimeTracking/Models/Service/SyncService.cs
@@ -38,9 +38,9 @@ namespace TimeTracking.Models
         }
         internal Syncdto GetDatafromDBEmployee(Syncdto syncObjects)
         {
-           
+
             List<Employee> empList = new List<Employee>();
-      
+
             using (SqlConnection sqlConnection = new SqlConnection(syncObjects.ConnectionString))
             {
                 string oString = "Select * from Employee";
@@ -56,7 +56,7 @@ namespace TimeTracking.Models
                         qboEmp.PrimaryPhone = new TelephoneNumber { FreeFormNumber = oReader["PrimaryPhone"].ToString() };
                         qboEmp.PrimaryEmailAddr = new EmailAddress { Address = oReader["PrimaryEmailAddr"].ToString() };
                         empList.Add(qboEmp);
-                       
+
                     }
                     sqlConnection.Close();
                 }
@@ -64,11 +64,11 @@ namespace TimeTracking.Models
             syncObjects.EmployeeList = empList;
             return syncObjects;
         }
-       
+
 
         internal Syncdto GetDatafromDBCustomer(Syncdto syncObjects)
         {
-           
+
             List<Customer> custList = new List<Customer>();
             using (SqlConnection sqlConnection = new SqlConnection(syncObjects.ConnectionString))
             {
@@ -94,7 +94,7 @@ namespace TimeTracking.Models
         }
         internal Syncdto GetDatafromDBItem(Syncdto syncObjects)
         {
-           
+
             List<Item> itemList = new List<Item>();
             using (SqlConnection sqlConnection = new SqlConnection(syncObjects.ConnectionString))
             {
@@ -124,7 +124,7 @@ namespace TimeTracking.Models
 
             foreach (Employee emp in syncObjects.EmployeeList)
             {
-                string EXISTING_EMPLOYEE_QUERY = string.Format("select * from employee where active = true and givenName = '{0}' and familyName = '{1}'", emp.GivenName, emp.FamilyName);
+                string EXISTING_EMPLOYEE_QUERY = string.Format("select * from employee where active = true and givenName = '{0}' and familyName = '{1}'", emp.GivenName.Trim(), emp.FamilyName.Trim());
                 QueryService<Employee> queryService = new QueryService<Employee>(dataserviceFactory.getServiceContext);
                 Employee resultFound = queryService.ExecuteIdsQuery(EXISTING_EMPLOYEE_QUERY).FirstOrDefault<Employee>();
                 if (resultFound == null)
@@ -141,7 +141,7 @@ namespace TimeTracking.Models
         {
             foreach (Customer customerItem in syncObjects.CustomerList)
             {
-                string EXISTING_CUSTOMER_QUERY = string.Format("select * from customer where active = true and givenName = '{0}' and familyName = '{1}'", customerItem.GivenName, customerItem.FamilyName);
+                string EXISTING_CUSTOMER_QUERY = string.Format("select * from customer where active = true and givenName = '{0}' and familyName = '{1}'", customerItem.GivenName.Trim(), customerItem.FamilyName.Trim());
                 QueryService<Customer> queryService = new QueryService<Customer>(dataserviceFactory.getServiceContext);
                 Customer resultFound = queryService.ExecuteIdsQuery(EXISTING_CUSTOMER_QUERY).FirstOrDefault<Customer>();
                 if (resultFound == null)
@@ -159,7 +159,7 @@ namespace TimeTracking.Models
         {
             foreach (Item ItemItem in syncObjects.ItemList)
             {
-                string EXISTING_ITEM_QUERY = string.Format("select * from Item where active = true and name = '{0}'", ItemItem.Name);
+                string EXISTING_ITEM_QUERY = string.Format("select * from Item where active = true and name = '{0}'", ItemItem.Name.Trim());
                 QueryService<Item> queryService = new QueryService<Item>(dataserviceFactory.getServiceContext);
                 Item resultFound = queryService.ExecuteIdsQuery(EXISTING_ITEM_QUERY).FirstOrDefault<Item>();
                 if (resultFound == null)
@@ -175,80 +175,96 @@ namespace TimeTracking.Models
 
         public Syncdto IsServiceItemSync(Syncdto syncObjects, SyncService service)
         {
+            Dictionary<string, bool> isSync = new Dictionary<string, bool>();
             var itemDataInDb = service.GetDatafromDBItem(syncObjects);
-            int indexIterator = 0;
+            
             for (int i = 0; i < itemDataInDb.ItemList.Count; i++)
             {
-                string EXISTING_ITEM_QUERY = string.Format("select * from Item where active = true and name = '{0}'", itemDataInDb.ItemList[i].Name);
+                string EXISTING_ITEM_QUERY = string.Format("select * from Item where active = true and name = '{0}'", itemDataInDb.ItemList[i].Name.Trim());
                 QueryService<Item> queryService = new QueryService<Item>(service.ServiceContext);
                 Item resultFound = queryService.ExecuteIdsQuery(EXISTING_ITEM_QUERY).FirstOrDefault<Item>();
                 if (resultFound != null)
                 {
                     itemDataInDb.ItemList[i].Id = resultFound.Id;
-                    indexIterator = i+1;
+                    isSync.Add(itemDataInDb.ItemList[i].Name, true);
+                }
+                else
+                {
+                    isSync.Add(itemDataInDb.ItemList[i].Name, false);
                 }
             }
-            if (itemDataInDb.ItemList.Count == indexIterator)
+            if (isSync.Where(x => x.Value == false).Any())
             {
-                itemDataInDb.IsServiceItemSync = true;
-              
+                itemDataInDb.IsServiceItemSync = false;
+
             }
             else
             {
-                itemDataInDb.IsServiceItemSync = false;
+                itemDataInDb.IsServiceItemSync = true;
             }
             return itemDataInDb;
         }
-
         public Syncdto IsCustSync(Syncdto syncObjects, SyncService service)
         {
+            Dictionary<string, bool> isSync = new Dictionary<string, bool>();
             var custDataInDb = service.GetDatafromDBCustomer(syncObjects);
-            int indexIterator = 0;
+           
             for (int i = 0; i < custDataInDb.CustomerList.Count; i++)
             {
-                string EXISTING_CUSTOMER_QUERY = string.Format("select * from customer where active = true and givenName = '{0}' and familyName = '{1}'", custDataInDb.CustomerList[i].GivenName, custDataInDb.CustomerList[i].FamilyName);
+                string EXISTING_CUSTOMER_QUERY = string.Format("select * from customer where active = true and givenName = '{0}' and familyName = '{1}'", custDataInDb.CustomerList[i].GivenName.Trim(), custDataInDb.CustomerList[i].FamilyName.Trim());
                 QueryService<Customer> queryService = new QueryService<Customer>(service.ServiceContext);
                 Customer resultFound = queryService.ExecuteIdsQuery(EXISTING_CUSTOMER_QUERY).FirstOrDefault<Customer>();
                 if (resultFound != null)
                 {
                     custDataInDb.CustomerList[i].Id = resultFound.Id;
-                    indexIterator = i+1;
+                    isSync.Add(custDataInDb.CustomerList[i].GivenName, true);
+                }
+                else
+                {
+                    isSync.Add(custDataInDb.CustomerList[i].GivenName, false);
                 }
             }
-            if (custDataInDb.CustomerList.Count == indexIterator)
+            if (isSync.Where(x => x.Value == false).Any())
             {
-                custDataInDb.IsCustomerSync = true;
+                custDataInDb.IsCustomerSync = false;
             }
             else
             {
-                custDataInDb.IsCustomerSync = false;
+                custDataInDb.IsCustomerSync = true;
             }
             return custDataInDb;
         }
 
         public Syncdto IsEmpSync(Syncdto syncObjects, SyncService service)
         {
+            Dictionary<string, bool> isSync = new Dictionary<string, bool>();
             var empDataInDb = service.GetDatafromDBEmployee(syncObjects);
-            int indexIterator = 0;
             for (int i = 0; i < empDataInDb.EmployeeList.Count; i++)
             {
-                string EXISTING_EMPLOYEE_QUERY = string.Format("select * from employee where active = true and givenName = '{0}' and familyName = '{1}'", empDataInDb.EmployeeList[i].GivenName, empDataInDb.EmployeeList[i].FamilyName);
+                string EXISTING_EMPLOYEE_QUERY = string.Format("select * from employee where active = true and givenName='{0}' and familyName= '{1}'", empDataInDb.EmployeeList[i].GivenName.Trim(), empDataInDb.EmployeeList[i].FamilyName.Trim());
+
                 QueryService<Employee> queryService = new QueryService<Employee>(service.ServiceContext);
                 Employee resultFound = queryService.ExecuteIdsQuery(EXISTING_EMPLOYEE_QUERY).FirstOrDefault<Employee>();
                 if (resultFound != null)
                 {
-                   empDataInDb.EmployeeList[i].Id = resultFound.Id;
-                   indexIterator = i+1;
+                    empDataInDb.EmployeeList[i].Id = resultFound.Id;
+                    //indexIterator = i+1;
+                    isSync.Add(empDataInDb.EmployeeList[i].GivenName, true);
+                }
+                else
+                {
+                    isSync.Add(empDataInDb.EmployeeList[i].GivenName, false);
                 }
             }
-            if (empDataInDb.EmployeeList.Count == indexIterator)
-            {
-                 empDataInDb.IsEmployeeSync = true;
-            }
-            else
+            if (isSync.Where(x => x.Value == false).Any())
             {
                 empDataInDb.IsEmployeeSync = false;
             }
+            else
+            {
+                empDataInDb.IsEmployeeSync = true;
+            }
+
             return empDataInDb;
         }
         #endregion
