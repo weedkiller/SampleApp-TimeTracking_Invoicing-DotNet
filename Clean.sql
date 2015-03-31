25ce0c9c36f91696d065f3a736e1e0796882a9c7
smathilakath <sumod_madhavan@intuit.com>
2015-03-31 16:39:44 +0530

    [+] Added clean to help with db

--- Clean.sql
+++ Clean.sql
@@ -3,7 +3,12 @@
 select * from OAuthTokens
 select * from Employee
 truncate table Employee
+select * from TimeActivity
 
 UPDATE Employee
-SET GivenName='William', FamilyName='Worfing'
+SET GivenName='Mouse', FamilyName='book'
 WHERE Id='1';
+
+UPDATE TimeActivity
+SET Invoice_QboId=2
+WHERE QboId=79;
